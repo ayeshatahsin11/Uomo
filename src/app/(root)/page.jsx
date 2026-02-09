79aaf9752dc9bb348735a92ff55d1b6d60405644
ayeshatahsin11 <ayeshatahsin1000@gmail.com>
2026-02-09 18:04:03 +0600

    Prettier text editor added

--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -1,15 +1,20 @@
-
-import Banner from '@/components/home/Banner'
-import React from 'react'
+import Banner from "@/components/home/Banner";
+import React from "react";
 
 const page = () => {
   return (
-  <>
- <Banner/>
-  
+    <>
+      <Banner />
+      <h1 className="mx-auto bg-red-700 px-20 text-white">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora minus
+        quidem assumenda ipsam ut laboriosam? Obcaecati totam, accusantium ut
+        at, fugiat enim doloremque corporis quasi, eligendi exercitationem
+        aspernatur voluptates! Quidem.
+      </h1>
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
+
 
