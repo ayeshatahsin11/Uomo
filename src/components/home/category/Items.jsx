340b437e2591f68168ea93c30507c278c5429fce
ayeshatahsin11 <ayeshatahsin1000@gmail.com>
2026-02-10 10:02:28 +0600

    Font text added

--- src/components/home/category/Items.jsx
+++ src/components/home/category/Items.jsx
@@ -4,11 +4,15 @@ const Items = ({ item, isFirst, isSecond }) => {
   return (
     <>
       <div
-        className={`mb-5 w-full bg-cover! bg-center! bg-no-repeat! ${isFirst && "col-span-2 row-span-2"} ${isSecond && "col-span-2"} `}
-        style={{ background: `url(${item.image.src})` }}
+        className={`font-jost mb-5 w-full bg-cover! bg-center! bg-no-repeat! p-10 ${isFirst && "col-span-2 row-span-2"} ${isSecond && "col-span-2"} `}
+        style={{ background: `url(${item?.image?.src})` }}
       >
-        <h4>{item.title}</h4>
-        <h2>{item.name}</h2>
+        <div className="flex h-full flex-col justify-end">
+          <h4 className="text-primary text-[14px] leading-6 font-normal">
+            {item.title}
+          </h4>
+          <h2 className="text-[26px] font-medium">{item.name}</h2>
+        </div>
       </div>
     </>
   );
